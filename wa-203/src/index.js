53d8451857590612bf06ea2d7b61a95c3a34b704
BozenaMatak <57918562+BozenaMatak@users.noreply.github.com>
2020-03-21 12:31:06 +0100

    Add files via upload

---
+++ wa-203/src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express()  // instanciranje aplikacije
+const port = 3000  // port na kojem će web server slušati
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+let studenti = [  
+    {oib: "12312349172", ime: "Hrvoje", prezime: "Horvat"},  
+    {oib: "82723412342", ime: "Ivana", prezime: "Ivić"},  
+    {oib: "97283742342", ime: "Stjepan", prezime: "Stjepanović"}, 
+  ]
+  
+  let rezultat = studenti.map (doc =>`${doc.ime} ${doc.prezime} (${doc.oib})`)
+  console.log(rezultat)
+  
+
+
+app.listen(port, () => console.log(`Slušam na portu ${port}!`))
