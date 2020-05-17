8350f565a93e9a5ae5fb6681524e6bbfae1a6c72
BozenaMatak <57918562+BozenaMatak@users.noreply.github.com>
2020-05-17 11:38:46 +0200

    Add files via upload

---
+++ aplikacija-frontend/src/views/mojprofil.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mojprofil">
+    <div class="info">
+      <h2><strong>User information</strong></h2>
+      <div class="okvir">
+        
+      </div>
+    </div>
+  </div>    
+</template>
+
+<style scoped>
+  
+   .mojprofil{
+    margin: 5%;
+    min-height: 700px;
+    
+  }
+
+  .info{
+    background-color:white;
+    width: 80%;
+    margin: 1% auto;
+    padding: 2% 0;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    box-shadow: 9px 9px 7px gray;
+  }
+  h2{
+    text-align: left;
+    margin: 0 8%;
+  }
+
+  .okvir{
+    border: rgb(233, 230, 230) 3px solid;
+    width: 70%;
+    margin: auto;
+    padding: 20% 0;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+
+</style>
